589e8d665fd1ea6711315aa98840daa4c81f9e54
Abood <Abood@abdulhadis-imac>
2018-12-21 17:29:46 +0100

    inital 2

---
+++ FueldistributionManagement/src/main/java/com/abulzahab/FueldistributionManagement/FueldistributionManagementApplication.java
@@ -0,0 +1,14 @@
+package com.abulzahab.FueldistributionManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FueldistributionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FueldistributionManagementApplication.class, args);
+	}
+
+}
+
